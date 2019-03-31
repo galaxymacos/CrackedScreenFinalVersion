f450706ac0353d61c281096a5a477ac69a0ba3da
Eddie <xiaoyefm@live.cn>
2019-03-31 16:06:44 -0400

    allarm trigger reset

---
+++ Assets/allarmTriggerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allarmTriggerScript : MonoBehaviour
+{
+    private AudioSource souce;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            souce.Play();
+        }
+    }
+
+    private void Start()
+    {
+        souce = GetComponent<AudioSource>();
+    }
+}
