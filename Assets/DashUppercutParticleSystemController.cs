dcae877b33e4df9fa4b22ccf3b077793172ccb0c
Ruan Xun <galaxymacos@gmail.com>
2019-04-02 19:40:44 -0400

    Polish Dash Uppercut Particle Effect

---
+++ Assets/DashUppercutParticleSystemController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashUppercutParticleSystemController : MonoBehaviour
+{
+    [SerializeField] private GameObject dashUppercutParticleEffectRight;
+
+    private float timeToDisappear = 0.55f;
+    private Vector3 rotationLeft = new Vector3(20.2f, -9.5f,-114.7f);
+    private Vector3 rotationRight = new Vector3(-196.029f,22.301f,-41.123f);
+    private float timeRemainToDisappear;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemainToDisappear > 0)
+        {
+            timeRemainToDisappear -= Time.deltaTime;
+            if (timeRemainToDisappear <= 0)
+            {
+                dashUppercutParticleEffectRight.SetActive(false);
+            }
+        }
+    }
+
+    public void EnableDashUppercutParticleEffect()
+    {
+        if (PlayerProperty.controller.isFacingRight)
+        {
+            dashUppercutParticleEffectRight.transform.localRotation = Quaternion.Euler(rotationRight);
+        }
+        else
+        {
+            dashUppercutParticleEffectRight.transform.localRotation = Quaternion.Euler(rotationLeft);
+        }
+        dashUppercutParticleEffectRight.SetActive(true);
+
+        timeRemainToDisappear = timeToDisappear;
+    }
+}
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -47,5 +47,6 @@ public class PlayerAnimator : MonoBehaviour
         }
 
     }
+    
 
 }
