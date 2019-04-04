e1ce85461b6b1e2e8864ce49eca2f2d6e78653a8
Ruan Xun <galaxymacos@gmail.com>
2019-04-04 14:15:46 -0400

    Add particle effect to counter attack, jump and double jump

---
+++ Assets/JumpParticleEffectSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpParticleEffectSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform placeToSpawn;
+    [SerializeField] private GameObject jumpParticleEffect;
+
+    public void SpawnJumpParticleEffect()
+    {
+        print("Spawn jump particle effect");
+        var jumpParticleEffectSpawned = Instantiate(jumpParticleEffect, placeToSpawn.position, Quaternion.identity);
+        jumpParticleEffectSpawned.transform.parent = null;
+    }
+}
