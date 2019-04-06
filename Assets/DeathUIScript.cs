a9105d094dd71f41d155d9b833db8d9c920552be
Eddie <xiaoyefm@live.cn>
2019-04-06 13:32:19 -0400

    Death UI Script Works

---
+++ Assets/DeathUIScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathUIScript : MonoBehaviour
+{
+    [SerializeField] GameObject deathUI;
+    public void Restart()
+    {
+        SceneManager.LoadSceneAsync("SchoolScene", LoadSceneMode.Additive);
+    }
+    public void Continue()
+    {
+        fenceScript.death = false;
+        deathUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
