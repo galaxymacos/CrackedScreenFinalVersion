0ebcaaf14ee771079327aec12b4356a02d0261ab
Eddie <xiaoyefm@live.cn>
2019-04-10 13:43:22 -0400

    FinalPuzzleUI

---
+++ Assets/finalPuzzleFixBug.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalPuzzleFixBug : MonoBehaviour
+{
+    [SerializeField] GameObject finalPuzzleUI;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            finalPuzzleUI.SetActive(true);
+
+        }
+    }
+
+    public void QuitFinalPuzzle()
+    {
+        finalPuzzleUI.SetActive(false);
+    }
+}
