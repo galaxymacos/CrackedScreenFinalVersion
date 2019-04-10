5beee7c3b33b5bd50ad9a0a3214aca48e846e44e
Eddie <xiaoyefm@live.cn>
2019-04-10 14:30:25 -0400

    Final Bug Puzzle Fixed

--- Assets/finalPuzzleFixBug.cs
+++ Assets/finalPuzzleFixBug.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class finalPuzzleFixBug : MonoBehaviour
 {
     [SerializeField] GameObject finalPuzzleUI;
+    [SerializeField] GameObject saveUI;
+    [SerializeField] public InputField bugFixedInput;
+    public bool bugFixedCorrect = false;
+
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
@@ -16,6 +21,24 @@ public class finalPuzzleFixBug : MonoBehaviour
 
     public void QuitFinalPuzzle()
     {
-        finalPuzzleUI.SetActive(false);
+        saveUI.SetActive(true);
+        //finalPuzzleUI.SetActive(false);
     }
+
+    public void Confirm()
+    {
+        if (bugFixedInput.text == "canGoTo3dWorld=false;")
+        {
+            bugFixedCorrect = true;
+            Debug.Log("Bug Has Been Fiexed");
+        }
+    }
+    public void SaveScripBotton()
+    {
+        saveUI.SetActive(false);
+        finalPuzzleUI.SetActive(false);
+        Confirm();
+    }
+
+
 }
