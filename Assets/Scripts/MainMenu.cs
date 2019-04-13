d02838f7564df10c31facfcefdc0d5f50d7de361
Eddie <xiaoyefm@live.cn>
2019-04-13 17:26:24 -0400

    Mainmenu Done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,13 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject startArm;
+    [SerializeField] GameObject optionArm;
+    [SerializeField] GameObject QuitArm;
     public void PlayGame()
     {
+        startArm.SetActive(true);
+        optionArm.SetActive(false);
+        QuitArm.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OptionArm()
+    {
+        startArm.SetActive(false);
+        optionArm.SetActive(true);
+        QuitArm.SetActive(false);
     }
 
     public void QuitGame()
     {
+        startArm.SetActive(false);
+        optionArm.SetActive(false);
+        QuitArm.SetActive(true);
         Debug.Log("Quit");
         Application.Quit();
     }
