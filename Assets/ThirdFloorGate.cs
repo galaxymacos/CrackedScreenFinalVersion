1f46af01a52f33469c108657b3bdd3bec92cb888
Chengyi Liao <lcy804585626@yahoo.ca>
2019-04-17 00:54:55 -0400

    Gate scripts add & prefab set to origin

---
+++ Assets/ThirdFloorGate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdFloorGate : EnemySpawnerComponent
+{
+    public override void OnEnemyDie()
+    {
+        Destroy(gameObject);
+    }
+}
